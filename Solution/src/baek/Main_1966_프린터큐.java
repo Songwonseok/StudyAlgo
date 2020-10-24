837040ed08c7e7f4e1565855428516e586a7d0b9
dnjst <dnjstjr0517@gmail.com>
2020-10-24 17:13:20 +0900

    BOJ 1966번 프린터 큐 : 자료구조(Queue)

--- Solution/src/baek/Main_1966_프린터큐.java
+++ Solution/src/baek/Main_1966_프린터큐.java
@@ -6,39 +6,13 @@ import java.util.*;
 public class Main_1966_프린터큐 {
 	public static boolean check(int[] values, int value){
         value = Math.abs(value);
-        for(int i=1;i<values.length;i++){
-            if(values[i] > 0 && i > value)
+        for(int i=value+1;i<values.length;i++){
+            if(values[i] > 0)
                 return false;
         }
         return true;
     }
-    public static int solution(int[] priorities, int location) {
-        int answer = 0;
-        int[] values = new int[10];
-        Queue<Integer> queue = new LinkedList<>();     
-        
-        for(int i=0;i<priorities.length;i++){
-            values[priorities[i]]++;
-        }
-        priorities[location] *= -1;
-        
-        for(int i=0;i<priorities.length;i++){
-            queue.add(priorities[i]);
-        }
-        while(!queue.isEmpty()){
-            int curr = queue.poll();
-            if(check(values, curr)){
-                answer++;
-                values[Math.abs(curr)]--;
-                if(curr < 0) 
-                    return answer;    
-            }else{
-                queue.add(curr);
-            }
-        }
-        
-        return answer;
-    }
+	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
@@ -52,7 +26,6 @@ public class Main_1966_프린터큐 {
 			
 			for(int i=0;i<N;i++)
 				priorities[i] = Integer.parseInt(st.nextToken());
-				
 			
 	        int[] values = new int[10];
 	        Queue<Integer> queue = new LinkedList<>();     
