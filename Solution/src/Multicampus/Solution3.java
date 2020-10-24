0d8740725b86429baa91a39a3f3bd356bde1d499
dnjst <dnjstjr0517@gmail.com>
2020-10-24 16:59:26 +0900

    멀캠 SW통합 공채 3번 : SQL

---
+++ Solution/src/Multicampus/Solution3.java
@@ -0,0 +1,11 @@
+package Multicampus;
+
+public class Solution3 {
+
+//	SELECT DISTINCT p.NAME AS name1, c.NAME AS name2
+//	FROM schedule AS s, professor AS p, course AS c 
+//	WHERE s.PROFESSOR_ID = p.ID 
+//	AND s.COURSE_ID = c.ID
+//	AND p.DEPARTMENT_ID != c.DEPARTMENT_ID;
+
+}
