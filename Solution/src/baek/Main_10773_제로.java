3a841e4ae07e0583e34ba3bdd3c7eed46bfb0819
songwonseok <dnjstjr0517@gmail.com>
2020-11-19 18:36:56 +0900

    BOJ 10773번 제로 : Stack

--- Solution/src/baek/Main_10773_제로.java
+++ Solution/src/baek/Main_10773_제로.java
@@ -9,7 +9,6 @@ public class Main_10773_제로 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int K = Integer.parseInt(br.readLine());
 		Stack<Integer> stack = new Stack<>();
-		
 		for(int i=0;i<K;i++) {
 			int num = Integer.parseInt(br.readLine());
 			if(num != 0) 
