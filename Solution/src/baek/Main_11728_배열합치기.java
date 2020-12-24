e7694883bd4d517689cb676b1c5e87c5cdc40d26
songwonseok <dnjstjr0517@gmail.com>
2020-12-24 17:45:15 +0900

    BOJ 11728번 배열 합치기: 정렬
    - merge 정렬 방식과 유사함

---
+++ Solution/src/baek/Main_11728_배열합치기.java
@@ -0,0 +1,47 @@
+package baek;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_11728_배열합치기 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken()), M = Integer.parseInt(st.nextToken());
+		
+		int[] leftArray = new int[N], rightArray = new int[M], answer = new int[N+M];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++) {
+			leftArray[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<M;i++) {
+			rightArray[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(leftArray);
+		Arrays.sort(rightArray);
+		
+		int idx=0, leftIdx=0, rightIdx = 0;
+		while(leftIdx < N && rightIdx < M) {
+			if(leftArray[leftIdx] <= rightArray[rightIdx]) {
+				answer[idx++] = leftArray[leftIdx++];
+			}else {
+				answer[idx++] = rightArray[rightIdx++];
+			}
+		}
+		while(leftIdx < N) answer[idx++] = leftArray[leftIdx++];
+		while(rightIdx < M) answer[idx++] = rightArray[rightIdx++];
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(int num: answer) {
+			sb.append(Integer.toString(num)+" ");
+		}
+		System.out.println(sb);
+	}
+}
