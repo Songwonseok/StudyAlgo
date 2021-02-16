181a1db55da128eb4dec964b07fc9fc6ae8b1b94
songwonseok <dnjstjr0517@gmail.com>
2021-02-16 23:41:22 +0900

    Programmers LS-1

---
+++ Solution/src/Programmers/Solution_LS1.java
@@ -0,0 +1,15 @@
+package Programmers;
+
+public class Solution_LS1 {
+	
+	public static int solution(String[] holidays, int k) {
+		int answer = 0;
+		
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		
+	}
+
+}
