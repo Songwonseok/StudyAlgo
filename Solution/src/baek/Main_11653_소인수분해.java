3d1105d9d2864623b03f969630a9e134fbbb38cf
Songwonseok <dnjstjr0517@gmail.com>
2021-06-27 12:25:27 +0900

    BOJ 11653번 소인수분해: 수학

---
+++ Solution/src/baek/Main_11653_소인수분해.java
@@ -0,0 +1,27 @@
+package baek;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_11653_소인수분해 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+
+		for (int i = 2, end = (int)Math.sqrt(N); i <= end; i++) {
+			while (N % i == 0) {
+				sb.append(i).append("\n");
+				N /= i;
+			}
+		}
+
+		if (N > 1) {
+			sb.append(N).append("\n");
+		}
+
+		System.out.print(sb.toString());
+	}
+}
